7d7f2462bb6335f45f87d1f4e5f9f499ec0afdf6
JuanB91 <juan.bessone@davinci.edu.ar>
2026-09-12 23:01:07 -0300

    mapeado 1.0 y relocation de spawn

--- Assets/Scripts/Clase 4/PlayerSpawner.cs
+++ Assets/Scripts/Clase 4/PlayerSpawner.cs
@@ -3,56 +3,65 @@ using Fusion;
 
 public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
 {
-    [SerializeField] private GameObject _playerPrefab;
+    // Prefab 0 = Player 1
+    // Prefab 1 = Player 2
+    [SerializeField] private GameObject[] _playerPrefabs;
 
-    //Spawn points para los distintos players
+    // Spawn 0 = Player 1
+    // Spawn 1 = Player 2
     [SerializeField] private Transform[] _spawnTransforms;
 
-    //bool interno que indica si ya hay la cantidad de jugadores necesaria para jugar (2 segun la consigna)
     private bool _initialized;
-    
-    //Se ejecuta por CADA cliente conectado
+
     public void PlayerJoined(PlayerRef player)
     {
         var playersCount = Runner.SessionInfo.PlayerCount;
-        
-        //si el primer cliente ya espero al 2do spawneo un prefab en la 1era posicion
+
+        // Si el primer cliente estaba esperando al segundo,
+        // creo al Player 1.
         if (_initialized && playersCount >= 2)
         {
             CreatePlayer(0);
             return;
         }
-        
-        //Si el cliente que entro, es el mismo cliente donde corre este codigo, entonces:
+
         if (player == Runner.LocalPlayer)
         {
-            //si el player count es menor a la cantidad minima, seteo initialized.
-
-            if(playersCount < 2 ) 
+            if (playersCount < 2)
             {
                 _initialized = true;
-            
             }
-
-            //sino crear un player en el spawn point correspondiente
-
-            else 
+            else
             {
-                CreatePlayer (playersCount - 1);
-            
+                // Segundo jugador -> índice 1
+                CreatePlayer(playersCount - 1);
             }
         }
     }
 
-    void CreatePlayer(int spawnPointIndex)
+    void CreatePlayer(int playerIndex)
     {
         _initialized = false;
 
-        //consigo la posicion y rotacion del spawn point correspondiente
+        if (playerIndex < 0 || playerIndex >= _playerPrefabs.Length)
+        {
+            Debug.LogError($"No existe un prefab para el jugador {playerIndex}");
+            return;
+        }
 
-        //spawneo el prefab en la posicion y rotacion correcta
+        if (playerIndex >= _spawnTransforms.Length)
+        {
+            Debug.LogError($"No existe un Spawn Point para el jugador {playerIndex}");
+            return;
+        }
 
+        GameObject prefab = _playerPrefabs[playerIndex];
+        Transform spawnPoint = _spawnTransforms[playerIndex];
 
-        Runner.Spawn(_playerPrefab, _spawnTransforms[spawnPointIndex].position, _spawnTransforms[spawnPointIndex].rotation);
+        Runner.Spawn(
+            prefab,
+            spawnPoint.position,
+            spawnPoint.rotation
+        );
     }
 }
